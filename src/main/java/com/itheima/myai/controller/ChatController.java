4ceef38d0c4e71f3f6e5d23f6187ac72b82aa426
tae2011 <332062091@qq.com>
2026-06-09 23:34:50 +0800

    更改

--- src/main/java/com/itheima/myai/controller/ChatController.java
+++ src/main/java/com/itheima/myai/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-//@RequiredArgsConstructor
+//@RequiredArgsConstructor。。。
 @RestController
 @RequestMapping("/ai")
 public class ChatController {
